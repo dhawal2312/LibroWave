9aeb3e711567bc1bbc1ddd755746cbf46ddcf235
dhawal2312 <dhawalsharma07@gmail.com>
2023-11-14 19:01:30 +0530

    displayed data of returned books

--- FRONTEND/src/app/returned-books/returned-books.component.ts
+++ FRONTEND/src/app/returned-books/returned-books.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { BookService } from '../services/book/book.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { forkJoin, map } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-returned-books',
@@ -6,5 +10,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./returned-books.component.css']
 })
 export class ReturnedBooksComponent {
+  public books:any =[];
+  constructor(private bookService:BookService,private spinner: NgxSpinnerService,private toastr: ToastrService){
+
+  }
+  ngOnInit(){
+    this.loadReturnedBooks();
+  }
+  
+  loadReturnedBooks(){
+    this.books=[];
+    this.spinner.show();
+    this.bookService.getReturnedBooks()?.subscribe((response:any) => {
+      const observables = response.map((returnedBook: any) =>
+      this.fetchBookDetails(returnedBook)
+    );
+      console.log("obs",observables);
+      
+    forkJoin(observables).subscribe(() => {
+      this.spinner.hide();
+      console.log("Book Info =", this.books);
+    });
+
+      this.spinner.hide();
+      console.log("Book Info=",this.books)
+    },(error)=>{
+      this.spinner.hide();
+      const errors = error.error.errors;
+      errors.forEach((element:any) => {
+        this.toastr.error(element.msg);
+      });
+    })
+    console.log(this.books);
+    
+  }
+  
+  fetchBookDetails(returnBook: any) {
+    this.books=[];
+    return this.bookService.getBookById(returnBook.book)?.pipe(
+      map((response:any) => {
+        console.log("RES", response);
+  
+        const bookData = {
+          bookDetail: response,
+          bookIssueInfo: returnBook,
+        };
+        console.log("BookData", bookData);
+  
+        this.books.push(bookData);
+      })
+    );
+  }
 
 }
--- FRONTEND/src/app/services/book/book.service.ts
+++ FRONTEND/src/app/services/book/book.service.ts
@@ -9,6 +9,7 @@ export class BookService {
   private booksUrl="http://localhost:5000/api/book/get-all-books";
   private getBookByIdURL="http://localhost:5000/api/book/get-book-by-id";
   private getIssuedBooksURL ="http://localhost:5000/api/users/get-issued-books";
+  private getReturnedBooksURL = "http://localhost:5000/api/users/get-returned-books"
   constructor(private http: HttpClient) {
 
    }
@@ -35,6 +36,15 @@ export class BookService {
     if(token){
       const headers = new HttpHeaders().set('x-auth-token',token);
       return this.http.get(this.getIssuedBooksURL, {headers})
+    }
+    return;
+   }
+
+   getReturnedBooks(){
+    const token=localStorage.getItem('token')
+    if(token){
+      const headers = new HttpHeaders().set('x-auth-token',token);
+      return this.http.get(this.getReturnedBooksURL, {headers})
     }
     return;
    }
